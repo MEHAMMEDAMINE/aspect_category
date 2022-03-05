4e5cf0ee21cd00f4657ac9244bd9b56cf3dd5fb7
MEHAMMEDAMINE <48122920+MEHAMMEDAMINE@users.noreply.github.com>
2022-03-05 19:25:23 +0100

    Update generate_semeval_QA_M.py

--- generate/generate_semeval_QA_M.py
+++ generate/generate_semeval_QA_M.py
@@ -1,60 +1,13 @@
 import os
 
-data_dir='../data/semeval2014/'
+data_dir='../data/HAAD./'
 
 dir_path = data_dir+'bert-pair/'
 if not os.path.exists(dir_path):
     os.makedirs(dir_path)
 
 with open(dir_path+"test_QA_M.csv","w",encoding="utf-8") as g:
-    with open(data_dir+"Restaurants_Test_Gold.xml","r",encoding="utf-8") as f:
-        s=f.readline().strip()
-        while s:
-            category=[]
-            polarity=[]
-            if "<sentence id" in s:
-                left=s.find("id")
-                right=s.find(">")
-                id=s[left+4:right-1]
-                while not "</sentence>" in s:
-                    if "<text>" in s:
-                        left=s.find("<text>")
-                        right=s.find("</text>")
-                        text=s[left+6:right]
-                    if "aspectCategory" in s:
-                        left=s.find("category=")
-                        right=s.find("polarity=")
-                        category.append(s[left+10:right-2])
-                        left=s.find("polarity=")
-                        right=s.find("/>")
-                        polarity.append(s[left+10:right-2])
-                    s=f.readline().strip()
-                if "price" in category:
-                    g.write(id+"\t"+polarity[category.index("price")]+"\t"+"what do you think of the price of it ?"+"\t"+text+"\n")
-                else:
-                    g.write(id + "\t" + "none" + "\t" + "what do you think of the price of it ?" + "\t" + text + "\n")
-                if "anecdotes/miscellaneous" in category:
-                    g.write(id+"\t"+polarity[category.index("anecdotes/miscellaneous")]+"\t"+"what do you think of the anecdotes of it ?"+"\t"+text+"\n")
-                else:
-                    g.write(id + "\t" + "none" + "\t" + "what do you think of the anecdotes of it ?" + "\t" + text + "\n")
-                if "food" in category:
-                    g.write(id+"\t"+polarity[category.index("food")]+"\t"+"what do you think of the food of it ?"+"\t"+text+"\n")
-                else:
-                    g.write(id + "\t" + "none" + "\t" + "what do you think of the food of it ?" + "\t" + text + "\n")
-                if "ambience" in category:
-                    g.write(id+"\t"+polarity[category.index("ambience")]+"\t"+"what do you think of the ambience of it ?"+"\t"+text+"\n")
-                else:
-                    g.write(id + "\t" + "none" + "\t" + "what do you think of the ambience of it ?" + "\t" + text + "\n")
-                if "service" in category:
-                    g.write(id+"\t"+polarity[category.index("service")]+"\t"+"what do you think of the service of it ?"+"\t"+text+"\n")
-                else:
-                    g.write(id + "\t" + "none" + "\t" + "what do you think of the service of it ?" + "\t" + text + "\n")
-            else:
-                s = f.readline().strip()
-
-
-with open(dir_path+"train_QA_M.csv","w",encoding="utf-8") as g:
-    with open(data_dir+"Restaurants_Train.xml","r",encoding="utf-8") as f:
+    with open(data_dir+"HAAD_Test_Gold.xml","r",encoding="utf-8") as f:
         s=f.readline().strip()
         while s:
             category=[]
@@ -76,25 +29,133 @@ with open(dir_path+"train_QA_M.csv","w",encoding="utf-8") as g:
                         right=s.find("/>")
                         polarity.append(s[left+10:right-1])
                     s=f.readline().strip()
-                if "price" in category:
-                    g.write(id+"\t"+polarity[category.index("price")]+"\t"+"what do you think of the price of it ?"+"\t"+text+"\n")
-                else:
-                    g.write(id + "\t" + "none" + "\t" + "what do you think of the price of it ?" + "\t" + text + "\n")
-                if "anecdotes/miscellaneous" in category:
-                    g.write(id+"\t"+polarity[category.index("anecdotes/miscellaneous")]+"\t"+"what do you think of the anecdotes of it ?"+"\t"+text+"\n")
-                else:
-                    g.write(id + "\t" + "none" + "\t" + "what do you think of the anecdotes of it ?" + "\t" + text + "\n")
-                if "food" in category:
-                    g.write(id+"\t"+polarity[category.index("food")]+"\t"+"what do you think of the food of it ?"+"\t"+text+"\n")
-                else:
-                    g.write(id + "\t" + "none" + "\t" + "what do you think of the food of it ?" + "\t" + text + "\n")
-                if "ambience" in category:
-                    g.write(id+"\t"+polarity[category.index("ambience")]+"\t"+"what do you think of the ambience of it ?"+"\t"+text+"\n")
-                else:
-                    g.write(id + "\t" + "none" + "\t" + "what do you think of the ambience of it ?" + "\t" + text + "\n")
-                if "service" in category:
-                    g.write(id+"\t"+polarity[category.index("service")]+"\t"+"what do you think of the service of it ?"+"\t"+text+"\n")
-                else:
-                    g.write(id + "\t" + "none" + "\t" + "what do you think of the service of it ?" + "\t" + text + "\n")
+                if "المزايا" in category:
+                    g.write(id+"\t"+polarity[category.index("المزايا")]+"\t"+"ما رأيك في المزايا ؟؟ا"+"\t"+text+"\n")
+               
+                if "الحبكة" in category:
+                    g.write(id+"\t"+polarity[category.index("الحبكة")]+"\t"+"ما رأيك في الحبكة ؟؟"+"\t"+text+"\n")
+                
+                if "المشاعر" in category:
+                    g.write(id+"\t"+polarity[category.index("المشاعر")]+"\t"+"ما رأيك في المشاعر؟؟"+"\t"+text+"\n")
+                
+                if "الاسلوب" in category:
+                    g.write(id+"\t"+polarity[category.index("الاسلوب")]+"\t"+" ما رأيك في الاسلوب ؟؟"+"\t"+text+"\n")
+                
+                if "السياق" in category:
+                    g.write(id+"\t"+polarity[category.index("السياق")]+"\t"+"ما رأيك في السياق؟؟ "+"\t"+text+"\n")
+      
+                if "السلبيات" in category:
+                    g.write(id+"\t"+polarity[category.index("السلبيات")]+"\t"+"ما رأيك في السلبيات؟؟"+"\t"+text+"\n")
+
+                if "المؤلف" in category:
+                   g.write(id+"\t"+polarity[category.index("المؤلف")]+"\t"+"ما رأيك في المؤلف ؟؟"+"\t"+text+"\n")
+                
+                if "التقييم" in category:
+                   g.write(id+"\t"+polarity[category.index("التقييم")]+"\t"+"ما رأيك في التقييم ؟؟"+"\t"+text+"\n")
+                
+                if "الاماكن" in category:
+                    g.write(id+"\t"+polarity[category.index("الاماكن")]+"\t"+"ما رأيك في الاماكن ؟؟"+"\t"+text+"\n")
+                
+                if "الطائفية" in category:
+                  g.write(id+"\t"+polarity[category.index("الطائفية")]+"\t"+"ما رأأيك في الطائفية ؟؟"+"\t"+text+"\n")
+                
+                if "الخاتمة" in category:
+                 g.write(id+"\t"+polarity[category.index("الخاتمة")]+"\t"+"ما رأيك في الخاتمة ؟؟"+"\t"+text+"\n")
+              
+                if "اللغات" in category:
+                 g.write(id+"\t"+polarity[category.index("اللغات")]+"\t"+"ما رأيك في اللغات ؟؟"+"\t"+text+"\n")
+               
+                if "الهوامش" in category:
+                 g.write(id+"\t"+polarity[category.index("الهوامش")]+"\t"+"ما رأيك في الهوامش ؟؟"+"\t"+text+"\n")
+                
+                if "الوقت" in category:
+                 g.write(id+"\t"+polarity[category.index("الوقت")]+"\t"+"ما رأيك في الوقت؟؟ "+"\t"+text+"\n")
+               
+                if "الاموال" in category:
+                 g.write(id+"\t"+polarity[category.index("الاموال")]+"\t"+"ما رأيك في الاموال ؟؟"+"\t"+text+"\n")
+                
+                if "اسلوب" in category:
+                 g.write(id+"\t"+polarity[category.index("اسلوب")]+"\t"+"ما رأيك في اسلوب ؟؟"+"\t"+text+"\n")
+               
+                if "مشاعر" in category:
+                 g.write(id+"\t"+polarity[category.index("مشاعر")]+"\t"+"ما رأيك في مشاعر ؟؟"+"\t"+text+"\n")
+                
+            else:
+                s = f.readline().strip()
+
+
+with open(dir_path+"train_QA_M.csv","w",encoding="utf-8") as g:
+    with open(data_dir+"HAAD_Train.xml","r",encoding="utf-8") as f:
+        s=f.readline().strip()
+        while s:
+            category=[]
+            polarity=[]
+            if "<sentence id" in s:
+                left=s.find("id")
+                right=s.find(">")
+                id=s[left+4:right-1]
+                while not "</sentence>" in s:
+                    if "<text>" in s:
+                        left=s.find("<text>")
+                        right=s.find("</text>")
+                        text=s[left+6:right]
+                    if "aspectCategory" in s:
+                        left=s.find("category=")
+                        right=s.find("polarity=")
+                        category.append(s[left+10:right-2])
+                        left=s.find("polarity=")
+                        right=s.find("/>")
+                        polarity.append(s[left+10:right-1])
+                    s=f.readline().strip()
+                if "المزايا" in category:
+                    g.write(id+"\t"+polarity[category.index("المزايا")]+"\t"+"ما رأيك في المزايا ؟؟"+"\t"+text+"\n")
+               
+                if "الحبكة" in category:
+                    g.write(id+"\t"+polarity[category.index("الحبكة")]+"\t"+"ما رأيك في الحبكة ؟؟"+"\t"+text+"\n")
+                
+                if "المشاعر" in category:
+                    g.write(id+"\t"+polarity[category.index("المشاعر")]+"\t"+"ما رأيك في المشاعر ؟؟"+"\t"+text+"\n")
+                
+                if "الاسلوب" in category:
+                    g.write(id+"\t"+polarity[category.index("الاسلوب")]+"\t"+"ما رأيك في الاسلوب ؟؟"+"\t"+text+"\n")
+                
+                if "السياق" in category:
+                    g.write(id+"\t"+polarity[category.index("السياق")]+"\t"+"ما رأيك في السياق ؟؟"+"\t"+text+"\n")
+      
+                if "السلبيات" in category:
+                    g.write(id+"\t"+polarity[category.index("السلبيات")]+"\t"+"ما رأيك في السلبيات ؟؟"+"\t"+text+"\n")
+
+                if "المؤلف" in category:
+                   g.write(id+"\t"+polarity[category.index("المؤلف")]+"\t"+"ما رأيك في المؤلف ؟؟"+"\t"+text+"\n")
+                
+                if "التقييم" in category:
+                   g.write(id+"\t"+polarity[category.index("التقييم")]+"\t"+" ما رأيك في التقييم ؟؟"+"\t"+text+"\n")
+                
+                if "الاماكن" in category:
+                    g.write(id+"\t"+polarity[category.index("الاماكن")]+"\t"+"ما رأأيك في الاماكن ؟؟"+"\t"+text+"\n")
+                
+                if "الطائفية" in category:
+                  g.write(id+"\t"+polarity[category.index("الطائفية")]+"\t"+"ما رأيك في الطائفية ؟؟"+"\t"+text+"\n")
+                
+                if "الخاتمة" in category:
+                 g.write(id+"\t"+polarity[category.index("الخاتمة")]+"\t"+"ما رأيك في الخاتمة ؟؟"+"\t"+text+"\n")
+              
+                if "اللغات" in category:
+                 g.write(id+"\t"+polarity[category.index("اللغات")]+"\t"+"ما رأيك في اللغات ؟؟"+"\t"+text+"\n")
+               
+                if "الهوامش" in category:
+                 g.write(id+"\t"+polarity[category.index("الهوامش")]+"\t"+" ما رأيك في الهوامش ؟؟"+"\t"+text+"\n")
+                
+                if "الوقت" in category:
+                 g.write(id+"\t"+polarity[category.index("الوقت")]+"\t"+"ما رأيك في الوقت ؟؟"+"\t"+text+"\n")
+               
+                if "الاموال" in category:
+                 g.write(id+"\t"+polarity[category.index("الاموال")]+"\t"+"ما رأيك في الاموال ؟؟"+"\t"+text+"\n")
+                
+                if "اسلوب" in category:
+                 g.write(id+"\t"+polarity[category.index("اسلوب")]+"\t"+"ما رأيك في اسلوب ؟؟"+"\t"+text+"\n")
+               
+                if "مشاعر" in category:
+                 g.write(id+"\t"+polarity[category.index("مشاعر")]+"\t"+"ما رأيك في مشاعر ؟؟"+"\t"+text+"\n")
             else:
                 s = f.readline().strip()
