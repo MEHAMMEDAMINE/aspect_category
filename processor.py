a0122cc9802de67b5264cfbc2dde37cf288068ca
MEHAMMEDAMINE <48122920+MEHAMMEDAMINE@users.noreply.github.com>
2022-03-07 09:10:50 +0100

    Update processor.py

--- processor.py
+++ processor.py
@@ -373,17 +373,29 @@ class Semeval_QA_M_Processor(DataProcessor):
 
     def get_train_examples(self, data_dir):
         """See base class."""
-        train_data = pd.read_csv(os.path.join(data_dir, "train_QA_M.csv"),header=None,sep="\t").values
+        train_data = pd.read_csv(os.path.join(data_dir, "train_NLI_M.csv"),header=None,sep="\t")
+        train_data = train_data.drop(train_data[train_data[1] == "none"].sample(frac=.85,random_state=0).index)
+        train_data=train_data.sample(frac = 1, random_state=0)
+        train_data=train_data.values
         return self._create_examples(train_data, "train")
 
     def get_dev_examples(self, data_dir):
         """See base class."""
-        dev_data = pd.read_csv(os.path.join(data_dir, "dev_QA_M.csv"),header=None,sep="\t").values
+        dev_data = pd.read_csv(os.path.join(data_dir, "dev_NLI_M.csv"),header=None,sep="\t")
+        dev_data = dev_data.drop(dev_data[dev_data[1] == "none"].sample(frac=.85,random_state=0).index)
+        dev_data=dev_data.sample(frac = 1, random_state=0)
+        dev_data=dev_data.values
         return self._create_examples(dev_data, "dev")
 
     def get_test_examples(self, data_dir):
         """See base class."""
-        test_data = pd.read_csv(os.path.join(data_dir, "test_QA_M.csv"),header=None,sep="\t").values
+        test_data = pd.read_csv(os.path.join(data_dir, "test_NLI_M.csv"),header=None,sep="\t")
+        n=4242-150
+        #test_data=test_data.drop(test_data[test_data[1].eq("none")].sample(n).index)
+        test_data = test_data.drop(test_data[test_data[1] == "none"].sample(frac=.80,random_state=0).index)
+        test_data=test_data.sample(frac = 1, random_state=0)
+
+        test_data=test_data.values
         return self._create_examples(test_data, "test")
 
     def get_labels(self):
