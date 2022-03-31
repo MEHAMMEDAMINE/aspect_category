13b41440d6ef97f087005ac2c66fa54da67c1af6
MEHAMMEDAMINE <48122920+MEHAMMEDAMINE@users.noreply.github.com>
2022-03-31 20:35:00 +0100

    Update processor.py

--- processor.py
+++ processor.py
@@ -375,7 +375,7 @@ class Semeval_QA_M_Processor(DataProcessor):
         """See base class."""
         train_data = pd.read_csv(os.path.join(data_dir, "train_QA_M.csv"),header=None,sep="\t")
      #    train_data = train_data.drop(train_data[train_data[1] == "none"].sample(frac=.85,random_state=0).index)
-    #    train_data=train_data.sample(frac = 1, random_state=0)
+        train_data=train_data.sample(frac = 1, random_state=0)
         train_data=train_data.values
         return self._create_examples(train_data, "train")
 
@@ -393,7 +393,7 @@ class Semeval_QA_M_Processor(DataProcessor):
      #    n=4242-150
         #test_data=test_data.drop(test_data[test_data[1].eq("none")].sample(n).index)
      #    test_data = test_data.drop(test_data[test_data[1] == "none"].sample(frac=.80,random_state=0).index)
-    #    test_data=test_data.sample(frac = 1, random_state=0)
+        test_data=test_data.sample(frac = 1, random_state=0)
 
         test_data=test_data.values
         return self._create_examples(test_data, "test")
